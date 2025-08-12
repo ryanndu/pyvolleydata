12e11ca747d8af0869f844b2eeb4b8115ade5398
Ryan <ryandu343@gmail.com>
2025-08-11 22:40:54 -0400

    Update docstrings and add load schedule function for lovb

--- src/pyvolleydata/lovb.py
+++ src/pyvolleydata/lovb.py
@@ -3,6 +3,60 @@ from . import helpers as h
 from datetime import datetime
 
 
+def load_lovb_schedule(seasons = None):
+    """
+    Load cleaned lovb schedule data from the volleydata repository.
+    
+    Parameters
+    ----------
+    seasons : int, list of int, or None, optional
+        Season(s) to load. By default, None loads all available seasons.
+        - int : Single season year (e.g., 2025)
+        - list of int : Multiple seasons (e.g., [2024, 2025])
+        - None : Load all available seasons
+
+        All years must be 2025 or later.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing the schedule with the following columns:
+
+        ================================  ===========
+        Column Name                        Type
+        ================================  ===========
+        season                             int
+        date                               string
+        round                              string
+        home_team                          string
+        away_team                          string
+        home_team_set_wins                 int
+        away_team_set_wins                 int
+        match_link                         string
+        secondary_link                     string
+        match_id                           int
+        ================================  ===========
+    
+    Examples
+    --------
+    >>> load_lovb_schedule(2025)
+    >>> load_lovb_schedule()
+    """
+
+    if isinstance(seasons, int):
+        seasons = [seasons]
+    if isinstance(seasons, list):
+        h.validate_seasons(seasons, league_start_year=2025)
+    elif seasons is None:
+        seasons = list(range(2025, datetime.now().year + 1))
+    else:
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
+    
+    schedule = pd.read_csv('https://github.com/awosoga/volleydata/releases/download/lovb-schedule/lovb_schedule.csv')
+    schedule = schedule[schedule['season'].isin(seasons)]
+    return schedule
+
+
 def load_lovb_officials(seasons = None):
     """
     Load cleaned lovb officials data from the volleydata repository.
@@ -15,7 +69,7 @@ def load_lovb_officials(seasons = None):
         - list of int : Multiple seasons (e.g., [2024, 2025])
         - None : Load all available seasons
 
-        All years must be 2024 or later.
+        All years must be 2025 or later.
 
     Returns
     -------
@@ -38,150 +92,21 @@ def load_lovb_officials(seasons = None):
     Examples
     --------
     >>> load_lovb_officials(2025)
-    >>> load_lovb_officials([2024, 2025])
     >>> load_lovb_officials()
     """
 
     if isinstance(seasons, int):
         seasons = [seasons]
     if isinstance(seasons, list):
-        h.validate_seasons(seasons, 2025)
+        h.validate_seasons(seasons, league_start_year=2025)
     elif seasons is None:
-        seasons = list(range(2024, datetime.now().year + 1))
+        seasons = list(range(2025, datetime.now().year + 1))
     else:
-        raise TypeError(f"Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}")
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
     
-    officials = pd.read_csv("https://github.com/awosoga/volleydata/releases/download/lovb-officials/lovb_officials.csv")
+    officials = pd.read_csv('https://github.com/awosoga/volleydata/releases/download/lovb-officials/lovb_officials.csv')
     officials = officials[officials['season'].isin(seasons)]
     return officials
-
-
-def load_lovb_points_log(seasons = None):
-    """
-    Load cleaned lovb point log data from the volleydata repository.
-
-    Parameters
-    ----------
-    seasons : int, list of int, or None, optional
-        Season(s) to load. By default, None loads all available seasons.
-        - int : Single season year (e.g., 2025)
-        - list of int : Multiple seasons (e.g., [2024, 2025])
-        - None : Load all available seasons
-
-        All years must be 2024 or later.
-
-    Returns
-    -------
-    pandas.DataFrame
-        A DataFrame containing the schedule with the following columns:
-
-        ================================  ===========
-        Column Name                        Type
-        ================================  ===========
-        match_id                           int
-        season                             int
-        match_datetime                     string
-        home_team_name                     string
-        away_team_name                     string
-        team_involved                      string
-        jersey_number                      int         
-        action                             string
-        outcome                            string
-        set                                int
-        point_number                       int
-        point_winner                       string
-        home_score                         int
-        away_score                         int                           
-        ================================  ===========
-    
-    Examples
-    --------
-    >>> load_lovb_points_log(2025)
-    >>> load_lovb_points_log([2024, 2025])
-    >>> load_lovb_points_log()
-    """
-
-    if isinstance(seasons, int):
-        seasons = [seasons]
-    if isinstance(seasons, list):
-        h.validate_seasons(seasons, 2025)
-    elif seasons is None:
-        seasons = list(range(2024, datetime.now().year + 1))
-    else:
-        raise TypeError(f"Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}")
-    
-    points_log = pd.read_csv("https://github.com/awosoga/volleydata/releases/download/lovb-points-log/lovb_points_log.csv")
-    points_log = points_log[points_log['season'].isin(seasons)]
-    return points_log
-
-
-def load_lovb_pbp(seasons = None):
-    """
-    Load cleaned lovb play-by-play data from the volleydata repository.
-
-    Parameters
-    ----------
-    seasons : int, list of int, or None, optional
-        Season(s) to load. By default, None loads all available seasons.
-        - int : Single season year (e.g., 2025)
-        - list of int : Multiple seasons (e.g., [2024, 2025])
-        - None : Load all available seasons
-
-        All years must be 2024 or later.
-
-    Returns
-    -------
-    pandas.DataFrame
-        A DataFrame containing the schedule with the following columns:
-
-        ================================  ===========
-        Column Name                        Type
-        ================================  ===========
-        match_id                           int
-        season                             int
-        match_datetime                     string
-        set                                int
-        set_start_time                     string
-        set_end_time                       string
-        set_duration                       int
-        set_home_score                     int
-        set_away_score                     int
-        event_type                         string
-        event_time                         string
-        libero_enters                      bool
-        team_involved                      string
-        libero_jersey_number               int
-        libero_subsitute_jersey_number     int
-        rally_start_time                   string
-        rally_end_time                     string
-        point_team                         string
-        call_approved                      bool
-        player_in_jersey_number            int
-        player_out_jersey_number           int
-        challenge_reason                   string
-        current_home_score                 int
-        current_away_score                 int
-        ================================  ===========
-    
-    Examples
-    --------
-    >>> load_lovb_pbp(2025)
-    >>> load_lovb_pbp([2024, 2025])
-    >>> load_lovb_pbp()
-    """
-
-    if isinstance(seasons, int):
-        seasons = [seasons]
-    if isinstance(seasons, list):
-        h.validate_seasons(seasons, 2025)
-    elif seasons is None:
-        seasons = list(range(2024, datetime.now().year + 1))
-    else:
-        raise TypeError(f"Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}")
-    
-    pbp = pd.read_csv("https://github.com/awosoga/volleydata/releases/download/lovb-pbp/lovb_pbp.csv")
-    pbp = pbp[pbp['season'].isin(seasons)]
-    return pbp
 
 
 def load_lovb_player_boxscore(seasons = None):
@@ -196,7 +121,7 @@ def load_lovb_player_boxscore(seasons = None):
         - list of int : Multiple seasons (e.g., [2024, 2025])
         - None : Load all available seasons
 
-        All years must be 2024 or later.
+        All years must be 2025 or later.
 
     Returns
     -------
@@ -239,20 +164,19 @@ def load_lovb_player_boxscore(seasons = None):
     Examples
     --------
     >>> load_lovb_pbp(2025)
-    >>> load_lovb_pbp([2024, 2025])
     >>> load_lovb_pbp()
     """
 
     if isinstance(seasons, int):
         seasons = [seasons]
     if isinstance(seasons, list):
-        h.validate_seasons(seasons, 2025)
+        h.validate_seasons(seasons, league_start_year=2025)
     elif seasons is None:
-        seasons = list(range(2024, datetime.now().year + 1))
+        seasons = list(range(2025, datetime.now().year + 1))
     else:
-        raise TypeError(f"Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}")
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
     
-    player_boxscore = pd.read_csv("https://github.com/awosoga/volleydata/releases/download/lovb-player-boxscore/lovb_player_boxscore.csv")
+    player_boxscore = pd.read_csv('https://github.com/awosoga/volleydata/releases/download/lovb-player-boxscore/lovb_player_boxscore.csv')
     player_boxscore = player_boxscore[player_boxscore['season'].isin(seasons)]
     return player_boxscore
 
@@ -269,7 +193,7 @@ def load_lovb_team_staff(seasons = None):
         - list of int : Multiple seasons (e.g., [2024, 2025])
         - None : Load all available seasons
 
-        All years must be 2024 or later.
+        All years must be 2025 or later.
 
     Returns
     -------
@@ -292,19 +216,171 @@ def load_lovb_team_staff(seasons = None):
     Examples
     --------
     >>> load_lovb_team_staff(2025)
-    >>> load_lovb_team_staff([2024, 2025])
     >>> load_lovb_team_staff()
     """
 
     if isinstance(seasons, int):
         seasons = [seasons]
     if isinstance(seasons, list):
-        h.validate_seasons(seasons, 2025)
+        h.validate_seasons(seasons, league_start_year=2025)
     elif seasons is None:
-        seasons = list(range(2024, datetime.now().year + 1))
+        seasons = list(range(2025, datetime.now().year + 1))
     else:
-        raise TypeError(f"Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}")
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
     
-    team_staff = pd.read_csv("https://github.com/awosoga/volleydata/releases/download/lovb-team-staff/lovb_team_staff.csv")
+    team_staff = pd.read_csv('https://github.com/awosoga/volleydata/releases/download/lovb-team-staff/lovb_team_staff.csv')
     team_staff = team_staff[team_staff['season'].isin(seasons)]
     return team_staff
+
+
+def load_lovb_pbp(seasons = None):
+    """
+    Load cleaned lovb pbp data from the volleydata repository.
+
+    Parameters
+    ----------
+    seasons : int, list of int, or None, optional
+        Season(s) to load. By default, None loads all available seasons.
+        - int : Single season year (e.g., 2025)
+        - list of int : Multiple seasons (e.g., [2024, 2025])
+        - None : Load all available seasons
+
+        All years must be 2025 or later.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing the schedule with the following columns:
+
+        ================================  ===========
+        Column Name                        Type
+        ================================  ===========
+        match_id                           int
+        season                             int
+        match_datetime                     string
+        home_team_name                     string
+        away_team_name                     string
+        team_involved                      string
+        jersey_number                      int         
+        action                             string
+        outcome                            string
+        set                                int
+        point_number                       int
+        point_winner                       string
+        home_score                         int
+        away_score                         int                           
+        ================================  ===========
+    
+    Examples
+    --------
+    >>> load_lovb_points_log(2025)
+    >>> load_lovb_points_log()
+    """
+
+    if isinstance(seasons, int):
+        seasons = [seasons]
+    if isinstance(seasons, list):
+        h.validate_seasons(seasons, league_start_year=2025)
+    elif seasons is None:
+        seasons = list(range(2025, datetime.now().year + 1))
+    else:
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
+    
+    pbp = pd.DataFrame()
+    for season in seasons:
+        pbp = pd.concat([pbp, pd.read_csv(f'https://github.com/awosoga/volleydata/releases/download/lovb-pbp/lovb_pbp_{season}.csv')])
+    return pbp
+
+
+def load_lovb_events_log(seasons = None):
+    """
+    Load cleaned lovb events log data from the volleydata repository.
+
+    Parameters
+    ----------
+    seasons : int, list of int, or None, optional
+        Season(s) to load. By default, None loads all available seasons.
+        - int : Single season year (e.g., 2025)
+        - list of int : Multiple seasons (e.g., [2024, 2025])
+        - None : Load all available seasons
+
+        All years must be 2025 or later.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing the schedule with the following columns:
+
+        ================================  ===========
+        Column Name                        Type
+        ================================  ===========
+        match_id                           int
+        season                             int
+        match_datetime                     string
+        set                                int
+        set_start_time                     string
+        set_end_time                       string
+        set_duration                       int
+        set_final_home_score               int
+        set_final_away_score               int
+        event_type                         string
+        event_time                         string
+        libero_enters                      bool
+        team_involved                      string
+        libero_jersey_number               int
+        libero_subsitute_jersey_number     int
+        rally_start_time                   string
+        rally_end_time                     string
+        rally_point_winner                 string
+        substitute_in_jersey_number        int
+        substitute_out_jersey_number       int
+        challenge_approved                 string
+        challenge_reason                   string
+        challenge_method                   string
+        challenge_response                 string
+        challenge_at_home_score            float
+        challenge_at_away_score            float
+        challenge_score_change             string
+        serving_team                       string
+        current_home_score                 float
+        current_away_score                 float
+        home_team_p1                       float
+        home_team_p2                       float
+        home_team_p3                       float
+        home_team_p4                       float
+        home_team_p5                       float
+        home_team_p6                       float
+        away_team_p1                       float
+        away_team_p2                       float
+        away_team_p3                       float
+        away_team_p4                       float
+        away_team_p5                       float
+        away_team_p6                       float
+        verified_time                      string
+        verified_method                    string
+        sanction_type                      string
+        sanction_remark                    float
+        staff_first_name                   string
+        staff_last_name                    string
+        staff_type                         string
+        ================================  ===========
+    
+    Examples
+    --------
+    >>> load_lovb_events_log(2025)
+    >>> load_lovb_events_log()
+    """
+
+    if isinstance(seasons, int):
+        seasons = [seasons]
+    if isinstance(seasons, list):
+        h.validate_seasons(seasons, league_start_year=2025)
+    elif seasons is None:
+        seasons = list(range(2025, datetime.now().year + 1))
+    else:
+        raise TypeError(f'Expected seasons to be an int, list of ints, or None, got {type(seasons).__name__}')
+    
+    events_log = pd.DataFrame()
+    for season in seasons:
+        events_log = pd.concat([events_log, pd.read_csv(f'https://github.com/awosoga/volleydata/releases/download/lovb-events-log/lovb_events_log_{season}.csv')])
+    return events_log
